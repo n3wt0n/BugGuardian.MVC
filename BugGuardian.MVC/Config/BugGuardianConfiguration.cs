120dba1933699d90ac95c5b248c84ff5a9f50ef7
Davide Benvegnù <davide.benvegnu@outlook.com>
2017-02-28 17:20:33 +0800

    New configuration settings management

---
+++ BugGuardian.MVC/Config/BugGuardianConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace DBTek.BugGuardian.MVC.Config
+{
+    public class BugGuardianConfiguration : System.Configuration.ConfigurationSection
+    {
+        private static string BugGuardianConfigurationSectionConst = "BugGuardianConfiguration";
+
+        /// <summary>
+        /// Returns an BugGuardianConfiguration instance
+        /// </summary>
+        public static BugGuardianConfiguration GetConfig()
+            => (BugGuardianConfiguration)System.Configuration.ConfigurationManager.GetSection(BugGuardianConfiguration.BugGuardianConfigurationSectionConst) ?? new BugGuardianConfiguration();
+
+        [System.Configuration.ConfigurationProperty("BugGuardianSettings")]
+        public BugGuardianSettingCollection BugGuardianSettings
+            => (BugGuardianSettingCollection)this["BugGuardianSettings"] ?? new BugGuardianSettingCollection();
+    }
+}
---
+++ BugGuardian.MVC/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace DBTek.BugGuardian.MVC.Helpers
+{
+    public class ConfigurationHelper
+    {
+        private const string DefaultCollectionName = "DefaultCollection";
+
+        public static string Url
+            => Config.ConfigurationSettings.AppSettings["Url"];
+
+        public static string Username
+           => Config.ConfigurationSettings.AppSettings["Username"];
+
+        public static string Password
+           => Config.ConfigurationSettings.AppSettings["Password"];
+
+        public static string CollectiontName
+           => Config.ConfigurationSettings.AppSettings["CollectiontName"] ?? DefaultCollectionName;
+
+        public static string ProjectName
+           => Config.ConfigurationSettings.AppSettings["ProjectName"];
+
+        public static bool AvoidMultipleReport
+           => bool.Parse(Config.ConfigurationSettings.AppSettings["AvoidMultipleReport"] ?? "true");
+    }
+}
